e55b3f77ae6267e53d3d5197ce309378dfcfba96
Thanarut Rungruangwisetchai <chokdeech9@gmail.com>
2024-11-28 23:20:36 +0700

    add student

--- my-app/src/app/components/students/students.component.ts
+++ my-app/src/app/components/students/students.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-students',
-  standalone: true,
-  imports: [],
   templateUrl: './students.component.html',
-  styleUrl: './students.component.css'
+  styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit {
+  students: any[] = [];
+  newStudent = { firstName: '', lastName: '', studentId: '', grade: '', classroom: '' };
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents(): void {
+    this.http.get<any[]>('http://localhost:3000/api/students').subscribe(data => {
+      this.students = data;
+    });
+  }
+
+  addStudent(): void {
+    this.http.post('http://localhost:3000/api/students', this.newStudent).subscribe(() => {
+      this.loadStudents(); // Refresh the list
+      this.newStudent = { firstName: '', lastName: '', studentId: '', grade: '', classroom: '' };
+    });
+  }
+
+  deleteStudent(id: number): void {
+    this.http.delete(`http://localhost:3000/api/students/${id}`).subscribe(() => {
+      this.loadStudents(); // Refresh the list
+    });
+  }
 }
---
+++ my-app/src/app/module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StudentsComponent } from './components/students/students.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { GradesComponent } from './components/grades/grades.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StudentsComponent,
+    TeachersComponent,
+    StaffComponent,
+    CoursesComponent,
+    GradesComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
