b2d15fd68451b6a314ceac0cd8a86ab253bab7ca
Thanarut Rungruangwisetchai <chokdeech9@gmail.com>
2024-11-28 23:13:54 +0700

    เพิ่มหัวข้อ

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StudentsComponent } from './components/students/students.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { GradesComponent } from './components/grades/grades.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'teachers', component: TeachersComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'grades', component: GradesComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
