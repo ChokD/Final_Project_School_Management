293a16497445bd3e97be3b1d9b941f5a1d489d03
napartussamo <0930301683m@gmail.com>
2024-12-07 01:31:45 +0700

    ใส่ CRUD ใน Grades
    Grades.css+Grades.html+Grades.ts

--- school-erp/src/app/grades/grades.component.ts
+++ school-erp/src/app/grades/grades.component.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Grades {
+  studentid: string;
+  subjectcode: string;
+  score: string;
+  grade: string;
+}
 
 @Component({
   selector: 'app-grades',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './grades.component.html',
-  styleUrls: ['./grades.component.css'],
+  styleUrls: ['./grades.component.css']
 })
-export class GradesComponent {}
+export class GradesComponent {
+  gradesList: Grades[] = [
+    { studentid: 'Student ID', subjectcode: 'Subject Code', score: '75', grade: 'B' }
+  ];
+  newgrades: Grades = { studentid: '', subjectcode: '', score: '', grade: ''};
+  editIndex: number | null = null;
+
+  addOrUpdateStaff() {
+    if (this.editIndex !== null) {
+      this.gradesList[this.editIndex] = this.newgrades;
+      this.editIndex = null;
+    } else {
+      this.gradesList.push(this.newgrades);
+    }
+    this.newgrades = { studentid: '', subjectcode: '', score: '', grade: '' };
+  }
+
+  editStaff(index: number) {
+    this.newgrades = { ...this.gradesList[index] };
+    this.editIndex = index;
+  }
+
+  deleteGrade(index: number) {
+    this.gradesList.splice(index, 1);
+  }
+
+  onSubmit() {
+    this.gradesList.push({ ...this.newgrades });
+    this.newgrades = { studentid: '', subjectcode: '', score: '', grade: ''};
+    setTimeout(() => {
+      const rows = document.querySelectorAll('tbody tr');
+      rows[rows.length - 1].classList.add('new-row');
+    }, 0);
+  }
+}
