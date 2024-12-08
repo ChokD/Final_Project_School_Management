62ca237c97cd0d17bfc94bd12a06dc378b3b379e
ChayutWalu <chayutwalunchodom@gmail.com>
2024-12-08 20:31:55 +0700

    ทำให้ข้อมูลยังคงอยู่ในตารางได้ (ไม่ใช่DB)

--- school-erp/src/app/staff/staff.component.ts
+++ school-erp/src/app/staff/staff.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,21 +17,24 @@ interface Staff {
   templateUrl: './staff.component.html',
   styleUrls: ['./staff.component.css']
 })
-export class StaffComponent {
-  staffList: Staff[] = [
-    { firstName: 'First Name', lastName: 'Last Name', id: '12345', role: 'Your Role', salary: '$$$' }
-  ];
+export class StaffComponent implements OnInit {
+  staffList: Staff[] = [];
   newStaff: Staff = { firstName: '', lastName: '', id: '', role: '', salary: '' };
   editIndex: number | null = null;
+
+  ngOnInit() {
+    this.loadStaffList();
+  }
 
   addOrUpdateStaff() {
     if (this.editIndex !== null) {
       this.staffList[this.editIndex] = this.newStaff;
       this.editIndex = null;
     } else {
-      this.staffList.push(this.newStaff);
+      this.staffList.push({ ...this.newStaff });
     }
     this.newStaff = { firstName: '', lastName: '', id: '', role: '', salary: '' };
+    this.saveStaffList();
   }
 
   editStaff(index: number) {
@@ -41,14 +44,21 @@ export class StaffComponent {
 
   deleteStaff(index: number) {
     this.staffList.splice(index, 1);
+    this.saveStaffList();
   }
 
   onSubmit() {
-    this.staffList.push({ ...this.newStaff });
-    this.newStaff = { firstName: '', lastName: '', id: '', role: '', salary: '' };
-    setTimeout(() => {
-      const rows = document.querySelectorAll('tbody tr');
-      rows[rows.length - 1].classList.add('new-row');
-    }, 0);
+    this.addOrUpdateStaff();
+  }
+
+  saveStaffList() {
+    localStorage.setItem('staffList', JSON.stringify(this.staffList));
+  }
+
+  loadStaffList() {
+    const savedList = localStorage.getItem('staffList');
+    if (savedList) {
+      this.staffList = JSON.parse(savedList);
+    }
   }
 }
